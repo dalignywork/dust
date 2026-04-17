fbc1d0c1bb3de880b4c8020f6c141dc7dfeecbca
ykmsd <yuka@dust.tt>
2026-04-17 14:32:30 +0200

    cleanup flag

--- sdks/js/src/types.ts
+++ sdks/js/src/types.ts
@@ -734,7 +734,6 @@ const WhitelistableFeaturesSchema = FlexibleEnumSchema<
   | "self_created_slack_app_connector_rollout"
   | "show_debug_tools"
   | "skill_builder_instructions_html"
-  | "slack_bot_mcp"
   | "slack_enhanced_default_agent"
   | "slack_message_splitting"
   | "slideshow"
